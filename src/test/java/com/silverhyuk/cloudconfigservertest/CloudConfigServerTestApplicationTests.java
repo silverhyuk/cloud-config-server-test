3a50b1451ddc0d2d5ba7fd3cefe7c84e1be42176
ehchoi <ehchoi@cafe24corp.com>
2020-02-20 18:04:54 +0900

    Initial commit

---
+++ src/test/java/com/silverhyuk/cloudconfigservertest/CloudConfigServerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silverhyuk.cloudconfigservertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
